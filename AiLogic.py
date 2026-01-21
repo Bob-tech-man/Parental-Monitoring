44397b95e7aab44f7e21de40f07abd0d0a4d8861
Bob-tech-man <adamsasson04@gmail.com>
2026-01-21 15:48:41 +0200

    Add files via upload
    Switched from GEMINI to OLLAMA

--- AiLogic.py
+++ AiLogic.py
@@ -1,20 +1,11 @@
-from google import genai
+import ollama
 import json
 
-key = "AIzaSyD1wVVh1Zp-cJk4N6KIJUaktCEdeU33Kp4"
-
 class AiModule:
-    def __init__(self, key):
-        self.key = key
-        self.model = "gemini-2.5-flash"
-    def activate_module(self):
-        client = genai.Client(api_key=self.key)
-        return client
+    def __init__(self):
+        self.model= 'gemma3:4b'
 
     def check_history(self, age, history):
-
-        client = self.activate_module()
-
         prompt = f"""
         אתה מנוע חישוב למערכת בקרת הורים. אין לך שיקול דעת עצמאי.
 
@@ -33,7 +24,7 @@ class AiModule:
         3. אם אין אף אתר בעייתי – החזר מערכים ריקים.
         4. אין להשתמש בידע כללי או ברשימות ברירת מחדל.
         5. כל אתר חייב להופיע בדיוק כפי שהופיע בקלט.
-        6. הסיבה לכל אתר תהיה קשורה ישירות לתוכן האתר.
+        6. הסיבה לכל אתר תהיה קשורה ישירות לתוכן האתר בכמה משפטים.
 
         מבנה הפלט:
         {{
@@ -42,22 +33,28 @@ class AiModule:
         }}
         """
 
-        response = client.models.generate_content(model="gemini-2.5-flash", contents=f"{prompt}")
-        return response.text
+        response = ollama.chat(
+            model=self.model,
+            format="json",
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        )
+
+        raw_text = response["message"]["content"]
+        parsed = parse_ai_json(raw_text)
+        return parsed
 
     def recommend_blocked_websites(self, age):
-        client = self.activate_module()
-
-
         prompt = f"""
                 אתה יועץ בינה מלאכותית למערכת בקרת הורים.
-        
+
         קלט:
         - גיל הילד: {age}
-        
+
         משימה:
         בהתאם לגיל הילד, הצע רשימה של אתרי אינטרנט ופלטפורמות מקוונות שיש לחסום כברירת מחדל עבור ילד בגיל זה.
-        
+
         כללים חשובים:
         1. החזר תשובה בפורמט JSON תקני בלבד, ללא טקסט נוסף וללא Markdown.
         2. אל תספק הסברים, נימוקים או סיבות לחסימה.
@@ -65,7 +62,7 @@ class AiModule:
         4. אל תכלול אתרים חינוכיים או אתרים המיועדים לילדים.
         5. כל אתר יופיע פעם אחת בלבד.
         6. אם אין אתרים מומלצים לחסימה – החזר מערך ריק.
-        
+
         מבנה הפלט המחייב:
         {{
           "websites_to_block": [
@@ -75,11 +72,20 @@ class AiModule:
         }}
         """
 
-        response = client.models.generate_content(model="gemini-2.5-flash", contents=f"{prompt}")
-        return response.text
+        response = ollama.chat(
+            model=self.model,
+            format="json",
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        )
+
+        raw_text = response["message"]["content"]
+        parsed = parse_ai_json(raw_text)
+        return parsed
+
 
 def parse_ai_json(response_text):
-
     response_text = response_text.strip()
 
     start = response_text.find("{")
@@ -96,3 +102,16 @@ def parse_ai_json(response_text):
         return None
 
 
+# def process_history(self, data, age):
+#     history_sites = []
+#
+#     for item in data:
+#         url = item.get("url", "")
+#         if url:
+#             domain = url.split("//")[-1].split("/")[0]
+#             history_sites.append(domain)
+#
+#     response = self.agent.check_history(age, history_sites)
+#     return response
+
+    # ADD THIS INTO SERVER!!!!!!!!!!!! SWITCH FROM OLD TO NEW
